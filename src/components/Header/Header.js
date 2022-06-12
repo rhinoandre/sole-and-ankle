952c743191a3e4d233a617ecf3a6420c4724ab96
Rhino <a.bazoli@smartrecruiters.com>
2022-06-12 10:11:19 +0200

    fix: header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,11 +27,17 @@ const Header = () => {
 };
 
 const MainHeader = styled.div`
-  padding: 0 32px;
+  display: flex;
+
+  padding: 16px 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  display: flex;
+  gap: 40px;
+  margin: auto;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
